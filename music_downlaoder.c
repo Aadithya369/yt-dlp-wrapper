a5f71828c01d6fefb6a39a5755036578ec5a05f1
aadithya369 <aadithyaanandan123@gmail.com>
2026-04-20 13:45:36 +0530

    initial commit

---
+++ music_downlaoder.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+int main() {
+  char link[1000];
+  char command[2000];
+  char file_name[200];
+  snprintf(file_name, sizeof(file_name),
+           "/home/aadithya/Desktop/side_projects/youtube_to_mp3/link.txt");
+  FILE *file = fopen(file_name, "r+");
+  if (file == NULL) {
+    perror("File cant be opened");
+    return 1;
+  }
+  while ((fgets(link, sizeof(link), file))) {
+    snprintf(command, sizeof(command), "yt-dlp -x %s -P /home/aadithya/Music/",
+             link);
+    system(command);
+  }
+  FILE *truncate_file = fopen(file_name, "w");
+  fclose(truncate_file);
+  fclose(file);
+  return 0;
+}
